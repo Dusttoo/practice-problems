e18dffa8d8cb172ced73890100ef82ab6cef40b2
Dusttoo <dusty.mumphrey@gmail.com>
2023-07-28 16:07:48 -0500

    working on alien invasion

---
+++ alien_invasion/ship.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Ship():
+    def __init__(self, screen):
+        """Initialize the ship and set its starting position."""
+        self.screen = screen
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        
